d4f4b4488ddf4c43158f9f462155ebb14715edbd
Blizz Softs <cagoh55@gmail.com>
2023-09-08 14:38:42 +0800

    Adding GUI

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 function getComputerSelection() {
-    // get number 1-3 that represent, 1:rock, 2:paper, 3:scissors
+    // get number 0-2 that represent, 1:rock, 2:paper, 3:scissors
     return getRandomInt()
 }
 
 function getPlayerSelection() {
     let playerSelectionString = prompt(`Round ${round++}: ROCK PAPER SCISSORS?`).toUpperCase();
     if (playerSelectionString === "ROCK")
-        return 1
+        return 0
     else if (playerSelectionString === "PAPER")
-        return 2
+        return 1
     else if (playerSelectionString === "SCISSORS")
-        return 3
+        return 2
 }
 
 // in javascript there is no built function to get random integer numbners
 function getRandomInt() {
     // In javascript, Math.random() gives floating number from 0-1, theres is no other built in to get random integer
-    return Math.floor(Math.random() * 3) + 1
+    return Math.floor(Math.random() * 2)
 }
 
 function game() {
@@ -39,15 +39,17 @@ function game() {
 }
 
 function playRound() {
-    let computerSelection = getComputerSelection()
-    let playerSelection = getPlayerSelection()
+    computerSelection = getComputerSelection()
+    // playerSelection = getPlayerSelection()
 
+    if (gameOver)
+        return
     if (playerSelection === computerSelection) {
         console.log("Draw! Fight again!")
     } else {
         switch (playerSelection) {
-            case 1:
-                if (computerSelection === 2) {
+            case 0:
+                if (computerSelection === 1) {
                     console.log("You Lose! Paper beats Rock")
                     computerScore++
                 }
@@ -56,8 +58,8 @@ function playRound() {
                     playerScore++
                 }
                 break
-            case 2:
-                if (computerSelection === 1) {
+            case 1:
+                if (computerSelection === 0) {
                     console.log("You Win! Paper beat Rock")
                     playerScore++
                 }
@@ -66,8 +68,8 @@ function playRound() {
                     computerScore++
                 }
                 break
-            case 3:
-                if (computerSelection === 1) {
+            case 2:
+                if (computerSelection === 0) {
                     console.log("You Lose! Rock beats Scissors")
                     computerScore++
                 }
@@ -80,10 +82,199 @@ function playRound() {
                 break
         }
     }
+    
+    // if (playerScore === 5) {
+
+    // } else if (computerScore === 5) {
+
+    // }
 }
+
+// Select the image by its ID
+const rockImg = document.getElementById("rock-img");
+const paperImg = document.getElementById("paper-img");
+const scissorsImg = document.getElementById("scissors-img");
+
+// Add a click event listener
+rockImg.addEventListener("click", function() {
+    // alert("Rock button clicked!");
+    playerSelection = 0;
+    playRound();
+    round++
+    if (playerScore < 5 && computerScore <  5) {
+        informationRound.innerHTML = `<p>Round: ${round}</p>`;
+        informationPlayed.innerHTML = `<p>Player: ${rockPaperScissorsArray[playerSelection]}, Computer: ${rockPaperScissorsArray[computerSelection]}</p>`;
+        informationRound.innerHTML = `<p>Round: ${round}</p>`;
+        informationScorePlayer.innerHTML = `<p>Your Score: ${playerScore}</p>`;
+        informationScoreComputer.innerHTML = `<p>Computer Score: ${computerScore}</p>`;
+    } else if (!gameOver) {
+        gameOver = true;
+        if (playerScore > computerScore) {
+            informationRound.innerHTML = '<br>';
+            informationScorePlayer.innerHTML = `<p>Your Score: ${playerScore}</p>`;
+            informationScoreComputer.innerHTML = `<p>Computer Score: ${computerScore}</p>`;
+            informationResult.innerHTML = '<p>Result: Player Wins!</p>';
+        }
+        else if (computerScore > playerScore) {
+            informationRound.innerHTML = '<br>';
+            informationScorePlayer.innerHTML = `<p>Your Score: ${playerScore}</p>`;
+            informationScoreComputer.innerHTML = `<p>Computer Score: ${computerScore}</p>`;
+            informationResult.innerHTML = '<p>Result: Computer Wins!</p>';
+        }
+        else {
+            informationRound.innerHTML = '<br>';
+            informationScorePlayer.innerHTML = `<p>Your Score: ${playerScore}</p>`;
+            informationScoreComputer.innerHTML = `<p>Computer Score: ${computerScore}</p>`;
+            informationResult.innerHTML = '<p>Result: Draw!</p>';
+        }
+        // Create a new button element
+        const refreshButton = document.createElement('button');
+        refreshButton.id = 'refresh-button'; // Set the button's ID
+        refreshButton.textContent = 'Play Again!'; // Set the button's text
+        refreshButton.style.fontSize = '24px';
+
+        // Add a click event listener to the button
+        refreshButton.addEventListener('click', function() {
+            location.reload(); // Reload the page when the button is clicked
+        });
+
+        refreshButtonContainer.appendChild(refreshButton)
+    }
+    
+});
+
+
+
+paperImg.addEventListener("click", function() {
+
+    // alert("Paper button clicked!");
+    playerSelection = 1
+    playRound()
+    round++
+    if (playerScore < 5 && computerScore <  5) {
+        informationRound.innerHTML = `<p>Round: ${round}</p>`;
+        informationPlayed.innerHTML = `<p>Player: ${rockPaperScissorsArray[playerSelection]}, Computer: ${rockPaperScissorsArray[computerSelection]}</p>`;
+        informationRound.innerHTML = `<p>Round: ${round}</p>`;
+        informationScorePlayer.innerHTML = `<p>Your Score: ${playerScore}</p>`;
+        informationScoreComputer.innerHTML = `<p>Computer Score: ${computerScore}</p>`;
+        
+    } else if (!gameOver) {
+        gameOver = true;
+        if (playerScore > computerScore) {
+            informationRound.innerHTML = '<br>';
+            informationScorePlayer.innerHTML = `<p>Your Score: ${playerScore}</p>`;
+            informationScoreComputer.innerHTML = `<p>Computer Score: ${computerScore}</p>`;
+            informationResult.innerHTML = '<p>Result: Player Wins!</p>';
+        }
+        else if (computerScore > playerScore) {
+            informationRound.innerHTML = '<br>';
+            informationScorePlayer.innerHTML = `<p>Your Score: ${playerScore}</p>`;
+            informationScoreComputer.innerHTML = `<p>Computer Score: ${computerScore}</p>`;
+            informationResult.innerHTML = '<p>Result: Computer Wins!</p>';
+        }
+        else {
+            informationRound.innerHTML = '<br>';
+            informationScorePlayer.innerHTML = `<p>Your Score: ${playerScore}</p>`;
+            informationScoreComputer.innerHTML = `<p>Computer Score: ${computerScore}</p>`;
+            informationResult.innerHTML = '<p>Result: Draw!</p>';
+        }
+        // Create a new button element
+        const refreshButton = document.createElement('button');
+        refreshButton.id = 'refresh-button'; // Set the button's ID
+        refreshButton.textContent = 'Play Again!'; // Set the button's text
+        refreshButton.style.fontSize = '24px';
+
+        // Add a click event listener to the button
+        refreshButton.addEventListener('click', function() {
+            location.reload(); // Reload the page when the button is clicked
+        });
+
+        refreshButtonContainer.appendChild(refreshButton)
+    }
+});
+
+
+
+scissorsImg.addEventListener("click", function() {
+
+    // alert("Scissors button clicked!");
+    playerSelection = 2
+    playRound()
+    round++
+    if (playerScore < 5 && computerScore <  5) {
+        informationRound.innerHTML = `<p>Round: ${round}</p>`;
+        informationPlayed.innerHTML = `<p>Player: ${rockPaperScissorsArray[playerSelection]}, Computer: ${rockPaperScissorsArray[computerSelection]}</p>`;
+        informationRound.innerHTML = `<p>Round: ${round}</p>`;
+        informationScorePlayer.innerHTML = `<p>Your Score: ${playerScore}</p>`;
+        informationScoreComputer.innerHTML = `<p>Computer Score: ${computerScore}</p>`;
+    } else if (!gameOver) {
+        gameOver = true;
+        if (playerScore > computerScore) {
+            informationRound.innerHTML = '<br>';
+            informationScorePlayer.innerHTML = `<p>Your Score: ${playerScore}</p>`;
+            informationScoreComputer.innerHTML = `<p>Computer Score: ${computerScore}</p>`;
+            informationResult.innerHTML = '<p>Result: Player Wins!</p>';
+        }
+        else if (computerScore > playerScore) {
+            informationRound.innerHTML = '<br>';
+            informationScorePlayer.innerHTML = `<p>Your Score: ${playerScore}</p>`;
+            informationScoreComputer.innerHTML = `<p>Computer Score: ${computerScore}</p>`;
+            informationResult.innerHTML = '<p>Result: Computer Wins!</p>';
+        }
+        else {
+            informationRound.innerHTML = '<br>';
+            informationScorePlayer.innerHTML = `<p>Your Score: ${playerScore}</p>`;
+            informationScoreComputer.innerHTML = `<p>Computer Score: ${computerScore}</p>`;
+            informationResult.innerHTML = '<p>Result: Draw!</p>';
+        }
+        // Create a new button element
+        const refreshButton = document.createElement('button');
+        refreshButton.id = 'refresh-button'; // Set the button's ID
+        refreshButton.textContent = 'Play Again!'; // Set the button's text
+        refreshButton.style.fontSize = '24px';
+
+        // Add a click event listener to the button
+        refreshButton.addEventListener('click', function() {
+            location.reload(); // Reload the page when the button is clicked
+        });
+
+        refreshButtonContainer.appendChild(refreshButton)
+
+    }
+
+});
+let gameOver = false;
 
 let playerScore=0
 let computerScore=0
 let round = 1
 
-game()
+let playerSelection
+let computerSelection
+
+let rockPaperScissorsArray = ['Rock', 'Paper', 'Scissors']
+
+// game()
+
+// // Select the <h1> and <h2> elements by their tag names
+// const h1Element = document.querySelector('.rock-paper-scissors-container h1');
+// const h2Element = document.querySelector('.rock-paper-scissors-container h2');
+
+// // Modify the content of the <h1> and <h2> elements
+// h1Element.textContent = 'New Heading';
+// h2Element.textContent = 'New Subheading';
+
+const informationPlayed = document.querySelector('#information-played');
+const informationRound = document.querySelector('#information-round');
+const informationScorePlayer = document.querySelector('#information-score-player');
+const informationScoreComputer = document.querySelector('#information-score-computer');
+const informationResult = document.querySelector('#information-result');
+const refreshButtonContainer = document.querySelector('#refresh-button-container');
+
+informationPlayed.innerHTML = `<p>Player: , Computer: </p>`;
+//informationPlayed.innerHTML = `<p>Player: ${rockPaperScissorsArray[playerSelection]}, Computer: ${rockPaperScissorsArray[computerSelection]}</p>`;
+//informationPlayed.innerHTML = `<p>Player: ${rockPaperScissorsArray[playerSelection]}, Computer: ${rockPaperScissorsArray[computerSelection]}</p>`;
+informationRound.innerHTML = `<p>Round: ${round}</p>`;
+informationScorePlayer.innerHTML = `<p>Your Score: ${playerScore}</p>`;
+informationScoreComputer.innerHTML = `<p>Computer Score: ${computerScore}</p>`;
+// informationResult.innerHTML = '<p>Result</p>';
